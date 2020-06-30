be6936f69931b2bc1f800f3c6d86f0facab8b63e
Wilfred Lopez <cubamc@gmail.com>
2020-06-29 21:50:55 -0400

    added final to variables and removed comments

--- src/App.java
+++ src/App.java
@@ -1,25 +1,16 @@
 
-// import java.util.Calendar;
 import java.util.Scanner;
 
 public class App {
-    // Implemented in a separate file. WilfredCalculator.java
-    // public static int getCurrentAge(int yob) {
-    // Calendar calendar = Calendar.getInstance();
-    // int currentYear = calendar.get(Calendar.YEAR);
-    // return currentYear - yob;
-    // }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         // variables
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
         System.out.print("Whats your name? ");
-        String name = scanner.nextLine().trim();
+        final String name = scanner.nextLine().trim();
         System.out.print("Whats your year of birth? ");
-        int yob = Integer.parseInt(scanner.nextLine().trim());
+        final int yob = Integer.parseInt(scanner.nextLine().trim());
         scanner.close();
-        // int age = App.getCurrentAge(yob);
-        int age = new WilfredCalculator().getCurrentAge(yob);
+        final int age = new WilfredCalculator().getCurrentAge(yob);
         System.out.println("Hello, " + name + "!\n" + "You are " + age + " years old.");
     }
 }
--- src/WilfredCalculator.java
+++ src/WilfredCalculator.java
@@ -1,9 +1,9 @@
 import java.util.Calendar;
 
 public class WilfredCalculator {
-    public int getCurrentAge(int yob) {
-        Calendar calendar = Calendar.getInstance();
-        int currentYear = calendar.get(Calendar.YEAR);
+    public int getCurrentAge(final int yob) {
+        final Calendar calendar = Calendar.getInstance();
+        final int currentYear = calendar.get(Calendar.YEAR);
         return currentYear - yob;
     }
 }
